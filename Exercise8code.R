81d1e4f1dac45346865ec934bd24908dca2fd182
loganjaglowski <loganjags11@gmail.com>
2019-11-22 08:49:10 -0500

    added data frame base and for loop for counting scores

--- Exercise8code.R
+++ Exercise8code.R
@@ -4,4 +4,22 @@ score=read.table("UWvMSU_1-22-13.txt", sep="\t", header=T, stringsAsFactors=NA)
 str(score)
 score
 #make vector for times a team scores
-time=unique(score$time)
+times=unique(score$time)
+#make data frame
+total=data.frame(time=times, UW=NA, MSU=NA)
+#make objects to track the teams' total scores
+UWtotal=0; MSUtotal=0
+#loop for times
+for (i in 1:length(times)){
+  curscore=score[score$time==times[i],]
+  for (j in 1:nrow(temp)){
+    #add scores to appropriate team's total with if else statement
+    if (curscore$team[j]=="UW"){
+      UWtotal=UWtotal+curscore$score[j]
+      }else{
+        MSUtotal=MSUtotal+curscore$score[j]
+      }
+  }
+  total$UW[i]=UWtotal
+  total$MSU[i]=MSUtotal
+  }
